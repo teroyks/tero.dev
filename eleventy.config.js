f96984488cd8e365fd559d1f8e08b1b4918095df
Tero Ykspetäjä <tero@valohai.com>
2026-02-09 15:11:54 +0200

    🔧 Use typographic quotes, ellipses, dashes automatically
    Get rid of the ugly 'computer typography' straight from the days of
    ASCII!

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,15 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
 
+  // convert straight quotes, ellipsis, and em and en dashes to typographic equivalents
+  // use '--' for en dashes and '---' for em dashes
+  eleventyConfig.amendLibrary("md", (mdLib) => {
+    mdLib.set({ typographer: true });
+  });
+
+  // custom "date" filter for templates
+  // usage: {{ date | date }} for "February 9, 2026"
+  //        {{ date | date: "%Y" }} for "2026" (used in blog permalinks)
   eleventyConfig.addFilter("date", (value, format) => {
     const date = new Date(value);
     if (format === "%Y") return date.getFullYear().toString();
